68d874e213122a83a6c65121085723b6ee819d7f
nukko <shouta.tsuboi@gmail.com>
2025-05-28 16:37:03 +0900

    URLを変更

--- background.js
+++ background.js
@@ -108,7 +108,7 @@ async function handleMessage(message, sendResponse) {
 			case 'FIND_OR_CREATE_STARTUP_TAB': {
 				// 既存のStartupタブを探す
 				const tabs = await chrome.tabs.query({
-					url: ['http://localhost:3000/*'],
+					url: ['https://startup.nukko.dev/*'],
 				})
 
 				if (tabs.length > 0) {
@@ -118,7 +118,7 @@ async function handleMessage(message, sendResponse) {
 				} else {
 					// 新しいタブを作成
 					const newTab = await chrome.tabs.create({
-						url: 'http://localhost:3000/',
+						url: 'https://startup.nukko.dev/',
 						active: true,
 					})
 					sendResponse({ success: true, tabId: newTab.id })
@@ -226,7 +226,7 @@ async function notifyTabsUpdate() {
 	try {
 		const tabs = await getAllTabs()
 		const matchingTabs = await chrome.tabs.query({
-			url: ['http://localhost:3000/*'],
+			url: ['https://startup.nukko.dev/*'],
 		})
 
 		for (const tab of matchingTabs) {
